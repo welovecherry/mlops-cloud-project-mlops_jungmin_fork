7f113049f6ec0b68ff47a8b44f8b795aae66f3d2
LucisObscu <tiqmfk123@naver.com>
2025-06-09 17:23:36 +0900

    feat : naver get wearher

--- mlops_team/data/utils/utils.py
+++ mlops_team/data/utils/utils.py
@@ -2,9 +2,10 @@
 
 import os
 import random
-
+import requests
 import numpy as np
-
+import json
+import re
 
 def init_seed():
     np.random.seed(0)
@@ -36,3 +37,19 @@ def auto_increment_run_suffix(name: str, pad=3):
     next_suffix = str(int(suffix) + 1).zfill(pad)
     return name.replace(suffix, next_suffix)
 
+def get_naver_weather():
+    req = requests.get('https://weather.naver.com/today/09140550')
+    pattern = r'"domesticWeeklyFcastList"\s*:\s*(\[\{.*?\}\])'
+    match = re.search(pattern, req.text, re.DOTALL)
+    json_str = match.group(1)
+    forecast_list = json.loads(json_str)
+    df = pd.DataFrame(forecast_list)
+
+    korean_keys = [
+    "지역코드", "시도", "시군구", "읍면동", "예보일", "예보일시",
+    "오전날씨코드", "오전날씨", "오전강수확률",
+    "오후날씨코드", "오후날씨", "오후강수확률",
+    "최저기온", "최고기온", "예보갱신시간", "요일"
+    ]
+    df.columns = korean_keys
+    return df
