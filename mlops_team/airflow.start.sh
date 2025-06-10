b1aef21f69a6bb59a84a53fb509ae8e70c3c015b
welovecherry <jungmin_hong@icloud.com>
2025-06-10 15:27:10 +0900

    fix: second add airflow into cicd

--- mlops_team/airflow.start.sh
+++ mlops_team/airflow.start.sh
@@ -11,17 +11,28 @@
 
 #!/bin/bash
 
+# airflow db init
+
+# airflow users create \
+#   --username admin \
+#   --firstname admin \
+#   --lastname lee \
+#   --role Admin \
+#   --email admin@airflow.com \
+#   --password admin123
+
+# # webserver와 scheduler를 모두 백그라운드 실행하지 말고, webserver만 foreground로
+# airflow scheduler &
+
+# exec airflow webserver -p 8181
+
+#!/bin/bash
 airflow db init
-
 airflow users create \
-  --username admin \
-  --firstname admin \
-  --lastname lee \
-  --role Admin \
-  --email admin@airflow.com \
-  --password admin123
-
-# webserver와 scheduler를 모두 백그라운드 실행하지 말고, webserver만 foreground로
-airflow scheduler &
-
-exec airflow webserver -p 8181
+    --username admin \
+    --firstname admin \
+    --lastname lee \
+    --role Admin \
+    --email admin@airflow.com \
+    --password admin123
+exec airflow webserver --host 0.0.0.0 --port 8181 & airflow scheduler
